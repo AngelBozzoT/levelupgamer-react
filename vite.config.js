28b5b43f2512f0458fd2ca8b7c14d1f61fc61a14
AngelBozzoT <angel.bozzo02@inacapmail.cl>
2026-07-09 01:42:54 -0400

    modificación para revisar página con vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./" // 👈 ESTA LÍNEA OBLIGA A VITE A USAR RUTAS RELATIVAS CORRECTAS EN CUALQUIER HOSTING
+  base: "/" // 👈 CAMBIADO A "/" PARA QUE VERCEL ENCUENTRE LOS ASSETS EN LA RAÍZ DE COMPILACIÓN
 })
