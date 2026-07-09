d4c8c6dfacb67869e1780d021d007a073258dd47
AngelBozzoT <angel.bozzo02@inacapmail.cl>
2026-07-09 01:40:47 -0400

    correción para revisar página con vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ← Asegúrate de que diga @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/levelupgamer-react/',
+  base: "./" // 👈 ESTA LÍNEA OBLIGA A VITE A USAR RUTAS RELATIVAS CORRECTAS EN CUALQUIER HOSTING
 })
